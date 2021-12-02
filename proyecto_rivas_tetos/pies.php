037b85a5455df207cbbe85d99c1b288289d5e62d
Cesar-Rivas <91627973+Cesar-Rivas@users.noreply.github.com>
2021-12-02 02:08:19 -0700

    Update pies.php

--- proyecto_rivas_tetos/pies.php
+++ proyecto_rivas_tetos/pies.php
@@ -4,6 +4,6 @@
             <h2> César René Rivas Andrade 5-I</h2>
             <h2> SUBMÓDULO 2:
                 Desarrolla aplicaciones Web con conexión a bases de datos</h2>
-            <p>https://github.com/elinava-web </p>
+            <a style="color:darkblue;" href="https://github.com/Cesar-Rivas/PROYECTO_FINAL_TETOS_RIVAS_5_I"><h3>https://github.com/Cesar-Rivas/PROYECTO_FINAL_TETOS_RIVAS_5_I</h3></a>
 		</div>
 	</footer>
